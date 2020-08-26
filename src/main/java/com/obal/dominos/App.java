6cd6bfbcd38579f8cbca1a6f9f0185d1324e1a69
thomas_gnv <thomas.bersez@geneviatechnologies.com>
2020-08-26 16:55:12 +0300

    Started the Snake class.

--- src/main/java/com/obal/dominos/App.java
+++ src/main/java/com/obal/dominos/App.java
@@ -5,39 +5,66 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 class Domino {
-    byte[] value;
+    byte[] values;
 
     public Domino(byte[] vl) {
-        value = vl;
+        values = vl;
     }
 
     void printValue() {
-        System.out.println(Arrays.toString(value));
+        System.out.println(Arrays.toString(values));
     }
 }
 
 class Hand {
-    LinkedList<Domino> dominos = new LinkedList<>();
+    LinkedList<Domino> dominoes = new LinkedList<>();
 
     void printHand() {
-        for (Domino domino : dominos)
-            System.out.print(Arrays.toString(domino.value));
+        for (Domino domino : dominoes)
+            System.out.print(Arrays.toString(domino.values));
         System.out.println();
     }
 
     void addDomino(Domino domino) {
-        dominos.addFirst(domino);
+        dominoes.addFirst(domino);
     }
     boolean isInHand(Domino domino) {
-        return dominos.contains(domino);
+        return dominoes.contains(domino);
     }
     void dropDomino(Domino domino) {
         if (isInHand(domino))
-            dominos.removeFirstOccurrence(domino);
+            dominoes.removeFirstOccurrence(domino);
         //TODO : ADD ERROR HANDLING
     }
 }
 
+class Snake {
+    // TODO : Find a better name for dominoes on the board haha
+    LinkedList<Domino> dominoes = new LinkedList<>();
+
+    void printSnake() {
+        for (Domino domino : dominoes)
+            System.out.print(Arrays.toString(domino.values));
+        System.out.println();
+    }
+
+    byte getRightValue() {
+        return dominoes.getLast().values[1];
+    }
+
+    byte getLeftValue() {
+        return dominoes.getFirst().values[0];
+    }
+
+    void layRightEnd(Domino domino) {
+
+    }
+
+    void layLeftEnd(Domino domino) {
+
+    }
+
+}
 public class App 
 {
     //CTS
@@ -70,6 +97,6 @@ public class App
         }
         playerOneHand.printHand();
         System.out.println("\nDeck size : " + deck.size());
-
     }
+
 }

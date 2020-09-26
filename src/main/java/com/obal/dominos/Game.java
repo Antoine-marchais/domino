da116a5b1be8c2bee1cbfc57d47ff87a4372bb5e
Antoine-marchais <antoine.marchais@student.ecp.fr>
2020-09-26 17:33:56 +0200

    Add Game and Player classes

---
+++ src/main/java/com/obal/dominos/Game.java
@@ -0,0 +1,9 @@
+package com.obal.dominos;
+
+public class Game {
+
+    Player firstPlayer;
+    Player secondPlayer;
+    Player thirdPlayer;
+    Snake snake;
+}
---
+++ src/main/java/com/obal/dominos/Player.java
@@ -0,0 +1,13 @@
+package com.obal.dominos;
+
+public class Player {
+    Hand hand;
+
+    public Player(Hand h) {
+        hand = h;
+    }
+
+    public Domino playNextMove(Snake snake){
+        //TODO
+    }
+}
